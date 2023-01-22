3fcdbe5b7accd14fca01eb98f420d4e8a98b96d0
Uwe Kristen <uk@boylesoftware.com>
2023-01-22 12:27:30 -0500

    Adds Gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,47 @@
+import type { GatsbyConfig } from 'gatsby';
+const dotenv = require('dotenv');
+dotenv.config({
+	path: `.env.${process.env.NODE_ENV}`,
+});
+const config: GatsbyConfig = {
+	siteMetadata: {
+		title: `bs23`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+	// If you use VSCode you can also use the GraphQL plugin
+	// Learn more at: https://gatsby.dev/graphql-typegen
+	graphqlTypegen: true,
+	plugins: [
+		{
+			resolve: 'gatsby-source-contentful',
+			options: {
+				spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+				accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+				host: process.env.GATSBY_CONTENTFUL_HOST,
+				environment: process.env.CONTENTFUL_ENVIRONMENT,
+				pageLimit: 200,
+			},
+		},
+		'gatsby-plugin-image',
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-sitemap',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				icon: 'src/images/icon.png',
+			},
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: './src/images/',
+			},
+			__key: 'images',
+		},
+	],
+};
+
+export default config;
