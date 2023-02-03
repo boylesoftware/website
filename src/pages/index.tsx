46d949e9e2c3a2a2d806d6bd68ec9d4edb373634
Uwe Kristen <uk@boylesoftware.com>
2023-02-02 20:31:05 -0500

    GraphQL query for homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,37 @@
 import * as React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+
 import type { HeadFC, PageProps } from 'gatsby';
 import * as s from './index.module.scss';
 
 const IndexPage: React.FC<PageProps> = () => {
-	return <main>hello matt, how are you today?</main>;
+	const {
+		contentfulHomepage: { content },
+	} = useStaticQuery(query);
+
+	return (
+		<main>
+			<h1>hello matt, how are you today?</h1>
+			{content.map((c: any) => (
+				<div key={c.id}>{c.title}</div>
+			))}
+		</main>
+	);
 };
+
+export const query = graphql`
+	query {
+		contentfulHomepage(contentful_id: { eq: "49ydOphXmgEVDUS2M9TyZg" }) {
+			content {
+				__typename
+				... on ContentfulSection {
+					id
+					title
+				}
+			}
+		}
+	}
+`;
 
 export default IndexPage;
 
