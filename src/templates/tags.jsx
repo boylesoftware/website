ee6a72865751bad8ae98780855c263b734e1a517
Daniela Chunga <daniela@boylesoftware.com>
2024-04-04 13:15:38 -0400

    Update code formatting to match

--- src/templates/tags.jsx
+++ src/templates/tags.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 // Components
-import { useStaticQuery, Link, graphql } from "gatsby"
+import { useStaticQuery, Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import { Image } from '../components/image';
 
