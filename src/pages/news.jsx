50265475e6f2a1f5ef3dd5b3786051cb1c00b2a2
Uwe Kristen <uk@boylesoftware.com>
2024-08-06 17:39:04 -0400

    Various accessibility fixes

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -2,47 +2,44 @@ import React from 'react';
 import { graphql, Link } from 'gatsby';
 import { renderRichText } from 'gatsby-source-contentful/rich-text';
 import { Image } from '../components/image';
+import { Seo } from '../components/seo';
 
 import * as styles from './news.module.scss';
 import Layout from '../components/layout';
 
 const NewsPage = ({ data: { allContentfulArticle } }) => (
 	<Layout>
-		<>
-			<div className={styles.pageHeader}>
-				<div className={styles.crumbs}></div>
-				{/* Keep the empty crumbs div for layout puropose */}
-				<h1>News</h1>
-				<div></div> {/* Keep this empty div for layout puropose */}
-			</div>
-			<section>
-				<ul className={styles.articleList}>
-					{allContentfulArticle.edges.map(({ node: article }) => (
-						<li key={article.id} className={styles.article}>
-							<div className={styles.articleImage}>
-								{article.image && (
-									<Link to={`/news/${article.slug}`}>
-										<Image
-											media={article.image}
-											alt={article.image.description}
-										/>
-									</Link>
-								)}
-							</div>
-							<div className={styles.articleInfo}>
-								<span className={styles.published}>{article.publishDate}</span>
-								<h2 className={styles.articleTitle}>
-									<Link to={`/news/${article.slug}`}>{article.title}</Link>
-								</h2>
-								<div>
-									{article.intro ? renderRichText(article.intro) : null}
-								</div>
-							</div>
-						</li>
-					))}
-				</ul>
-			</section>
-		</>
+		<div className={styles.pageHeader}>
+			<div className={styles.crumbs}></div>
+			{/* Keep the empty crumbs div for layout puropose */}
+			<h1>News</h1>
+			<div></div> {/* Keep this empty div for layout puropose */}
+		</div>
+		<section>
+			<ul className={styles.articleList}>
+				{allContentfulArticle.edges.map(({ node: article }) => (
+					<li key={article.id} className={styles.article}>
+						<div className={styles.articleImage}>
+							{article.image && (
+								<Link to={`/news/${article.slug}`}>
+									<Image
+										media={article.image}
+										alt={article.image.description || article.image.title}
+									/>
+								</Link>
+							)}
+						</div>
+						<div className={styles.articleInfo}>
+							<span className={styles.published}>{article.publishDate}</span>
+							<h2 className={styles.articleTitle}>
+								<Link to={`/news/${article.slug}`}>{article.title}</Link>
+							</h2>
+							<div>{article.intro ? renderRichText(article.intro) : null}</div>
+						</div>
+					</li>
+				))}
+			</ul>
+		</section>
 	</Layout>
 );
 
@@ -60,6 +57,7 @@ export const query = graphql`
 					}
 					image {
 						gatsbyImageData(width: 325, placeholder: BLURRED)
+						title
 						description
 						file {
 							url
@@ -70,5 +68,9 @@ export const query = graphql`
 		}
 	}
 `;
+
+export const Head = () => (
+	<Seo title='News' description='Listing of our news articles' />
+);
 
 export default NewsPage;
